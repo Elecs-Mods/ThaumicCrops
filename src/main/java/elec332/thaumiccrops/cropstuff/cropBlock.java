f035d143cd9a25351595793aa2de312960c97a1c
Elec332 <arnout1998@gmail.com>
2015-01-05 18:22:32 +0100

    Edited cropblock
    you can no longer bonemeal the crop&
    lowered the chance for seeds&crop drops

--- src/main/java/elec332/thaumiccrops/cropstuff/cropBlock.java
+++ src/main/java/elec332/thaumiccrops/cropstuff/cropBlock.java
@@ -5,7 +5,13 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import elec332.core.util.blocks.baseCrop;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Elec332 on 29-12-2014.
@@ -42,5 +48,49 @@ public class cropBlock extends baseCrop {
             this.icon[i] = IIconRegister.registerIcon("thaumiccrops:AspectCrop" + "_stage_" + i);
         }
         this.icon[7] = IIconRegister.registerIcon(this.getTextureName() + "_stage_" + 7);
+    }
+
+    @Override
+    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune)
+    {
+        ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+
+        if(metadata != 7) ret.add(new ItemStack(func_149866_i()));
+
+        if (metadata >= 7)
+        {
+            for (int i = 0; i < 1 + fortune; ++i)
+            {
+                if (world.rand.nextInt(9) <= metadata)
+                {
+                    ret.add(new ItemStack(this.func_149866_i(), 1));
+                }
+                if (world.rand.nextInt(18) <= metadata)
+                {
+                    ret.add(new ItemStack(this.func_149866_i(), 1));
+                }
+                if (world.rand.nextInt(9) <= metadata)
+                {
+                    ret.add(new ItemStack(func_149865_P()));
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    @Override
+    public Item getItemDropped(int metadata, Random random, int p_149650_3_)
+    {
+        //if (metadata != 7){
+        //    return func_149866_i();
+        //}
+        return null;
+    }
+
+    @Override
+    public boolean func_149851_a(World p_149851_1_, int p_149851_2_, int p_149851_3_, int p_149851_4_, boolean p_149851_5_)
+    {
+        return false;
     }
 }
