e5e6b861233064f82b0d62ee809fa7c0dff950d2
Elec332 <arnout1998@gmail.com>
2015-03-22 18:16:24 +0100

    Fix class name

---
+++ src/main/java/elec332/thaumiccrops/thaumcraft/lib/Lib.java
@@ -0,0 +1,10 @@
+package elec332.thaumiccrops.thaumcraft.lib;
+
+/**
+ * Created by Elec332 on 1-1-2015.
+ */
+public class Lib {
+    public static String TCTAB = "THAUMCROPS";
+    public static String PRIMALSEEDS = "PRIMALASPSEEDS";
+    public static String AGENT = "AGENT";
+}
